3aafc726b15946e3f0f8d7260802816e4135d2de
Almir Neto <almirneto338@gmail.com>
2024-01-24 21:19:11 -0300

    Adding base files and some base models

---
+++ BankChurnDataset.py
@@ -0,0 +1,12 @@
+from torch.utils.data import Dataset
+
+
+class BankChurnDataset(Dataset):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def __len__(self):
+        pass
+
+    def __getitem__(self, index):
+        pass
---
+++ ChurnModel.py
@@ -0,0 +1,9 @@
+from torch.nn import Module
+
+
+class ChurnModel(Module):
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+
+    def forward(self, x):
+        pass
