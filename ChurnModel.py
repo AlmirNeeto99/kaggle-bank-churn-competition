fb9fadd6e67b3a00d1e65c596159c34927f428fa
Almir Neto <almirneto338@gmail.com>
2024-01-24 23:57:15 -0300

    Trying to improve accuracy by changing activation function

--- ChurnModel.py
+++ ChurnModel.py
@@ -7,7 +7,7 @@ class ChurnModel(nn.Module):
         self.l1 = nn.Linear(10, 10240)
         self.a1 = nn.ReLU()
         self.l2 = nn.Linear(10240, 160)
-        self.a2 = nn.SiLU()
+        self.a2 = nn.ReLU()
         self.l3 = nn.Linear(160, 1)
         self.a3 = nn.Sigmoid()
 
