1198f4fb0561b5593b3b4b6b41cad56522115915
Almir Neto <almirneto338@gmail.com>
2024-01-25 00:32:49 -0300

    Moving data to device

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
+import torch
 from torch.optim import SGD
 from ChurnModel import ChurnModel
 from torch.nn import Unflatten, MSELoss
 from torch.utils.data import DataLoader
 from BankChurnDataset import BankChurnDataset
 
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+print(f"Using '{device}'")
+
 dataset = BankChurnDataset("data/train.csv")
 loader = DataLoader(dataset, 64)
 
-model = ChurnModel()
+model = ChurnModel().to(device)
 
 loss = MSELoss()
 optim = SGD(model.parameters(), 1e-9)
 
-EPOCHS = 3
+EPOCHS = 200
 SIZE = len(loader.dataset)
 
 model.train()
@@ -22,6 +28,7 @@ for i in range(EPOCHS):
     print(f"=== Running EPOCH {i+1}/{EPOCHS} ===\n")
 
     for idx, (inp, target) in enumerate(loader):
+        inp, target = inp.to(device), target.to(device)
         pred = model(inp)
 
         unflat = Unflatten(0, (len(target), 1))
@@ -45,9 +52,10 @@ data = pd.DataFrame({"id": [], "Exited": []})
 
 model.eval()
 for idx, (id, inp) in enumerate(loader):
+    id, inp = id.to(device), inp.to(device)
     out = model(inp)
 
-    data.loc[len(data)] = [id, out]
+    data.loc[len(data)] = [id.item(), out.item()]
 
 
-data.to_csv("submission.csv")
+data.to_csv("submission.csv", columns=["id", "Exited"], index=False)
